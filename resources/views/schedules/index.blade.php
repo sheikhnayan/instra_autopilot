6a2dc2f589f85348b48afd24676b7099d0e65600
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-09-05 03:03:01 +0600

    te

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -58,7 +58,7 @@
                             <svg class="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z"></path>
                             </svg>
-                            Post {{ $schedule->current_post_index + 1 }} of {{ $schedule->contentContainer->posts->count() ?? 0}}
+                            {{-- Post {{ $schedule->current_post_index + 1 }} of {{ $schedule->contentContainer->posts->count() ?? 0}} --}}
                         </div>
                     </div>
                 </div>
