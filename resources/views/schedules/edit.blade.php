71cfc7381603e9dd305c3dc3cfccf7cb2faa7298
sheikh Nayan <sheikhnayan1997@gmail.com>
2025-09-07 07:27:07 +0600

    test

--- resources/views/schedules/edit.blade.php
+++ resources/views/schedules/edit.blade.php
@@ -64,7 +64,7 @@
                             <label for="start_time" class="block text-sm font-medium text-gray-700 mb-2">Start Time</label>
                             <input type="time" id="start_time" name="start_time" required
                                    class="w-full px-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-                                   value="{{ old('start_time', $schedule->start_time ? $schedule->start_time->format('H:i') : '') }}">
+                                   value="{{ old('start_time', $schedule->start_time ?  \Carbon\Carbon::parse($schedule->start_time)->format('H:i') : '') }}">
                             @error('start_time')
                                 <p class="text-red-600 text-sm mt-1">{{ $message }}</p>
                             @enderror
